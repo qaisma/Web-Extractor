5cd7ffd22229dc35a63ee0d67afb7b16bd17bea1
qaisma <iamqays@gmail.com>
2021-03-16 22:13:00 +0100

    format

--- Web Extractor/Models/ExtractedObject.cs
+++ Web Extractor/Models/ExtractedObject.cs
@@ -12,6 +12,7 @@ namespace Web_Extractor.Models
             RoomCategories = new List<string>();
             AlternativeHotels = new List<string>();
         }
+
         public string HotelName { get; set; }
         public string Address { get; set; }
         public int Classification { get; set; }
